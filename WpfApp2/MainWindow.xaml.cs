c1289a10b1f205f43462ae47d4ea73dea3d9c395
Dmitriy Larionov <larionov.d.y@gmail.com>
2022-01-13 13:36:55 +0300

    done

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        
+        private void Button1ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_1.jpg", UriKind.Relative));
+        }
+
+        private void Button2ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_2.jpg", UriKind.Relative));
+        }
+
+        private void Button3ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_3.jpg", UriKind.Relative));
+        }
+
+        private void Button4ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_4.jpg", UriKind.Relative));
+        }
+
+        private void Button5ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_5.jpg", UriKind.Relative));
+        }
+
+        private void Button6ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_6.jpg", UriKind.Relative));
+        }
+
+        private void Button7ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_7.jpg", UriKind.Relative));
+        }
+
+        private void Button8ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_8.jpg", UriKind.Relative));
+        }
+
+        private void Button9ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_9.jpg", UriKind.Relative));        }
+
+        private void Button10ClickExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            album.Source = new BitmapImage(new Uri("Data/_10.jpg", UriKind.Relative));
+        }
     }
 }
---
+++ WpfApp2/MyCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    class MyCommands
+    {
+        public static RoutedCommand Button1Click { get; set; }
+        public static RoutedCommand Button2Click { get; set; }
+        public static RoutedCommand Button3Click { get; set; }
+        public static RoutedCommand Button4Click { get; set; }
+        public static RoutedCommand Button5Click { get; set; }
+        public static RoutedCommand Button6Click { get; set; }
+        public static RoutedCommand Button7Click { get; set; }
+        public static RoutedCommand Button8Click { get; set; }
+        public static RoutedCommand Button9Click { get; set; }
+        public static RoutedCommand Button10Click { get; set; }
+        static MyCommands()
+        {
+            Button1Click = new RoutedCommand();
+            Button2Click = new RoutedCommand();
+            Button3Click = new RoutedCommand();
+            Button4Click = new RoutedCommand();
+            Button5Click = new RoutedCommand();
+            Button6Click = new RoutedCommand();
+            Button7Click = new RoutedCommand();
+            Button8Click = new RoutedCommand();
+            Button9Click = new RoutedCommand();
+            Button10Click = new RoutedCommand();
+        }
+    }
+}
